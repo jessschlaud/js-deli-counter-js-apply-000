5485ca3c83962fc6750d5e7e9b0b69bb71ea0d67
Jessica Schlauderaff <jessicaschlauderaff@gmail.com>
2017-09-10 19:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var katzDeliLine = []
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if(katzDeliLine.length == 0) {
     return "There is nobody waiting to be served!"
   }
