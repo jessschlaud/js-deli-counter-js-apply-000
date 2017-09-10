5f11f25f0e0aa4ba5e7be3edf03db33864daa478
Jessica Schlauderaff <jessicaschlauderaff@gmail.com>
2017-09-10 19:55:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
+
+function nowServing() {
+  string nextCustomer = katzDeliLine[0]
+  katzDeliLine.shift()
+  return nextCustomer
+}
