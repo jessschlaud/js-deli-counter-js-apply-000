5a6fc00708eeaa5bfd8e04afeb1676cd2289dbb6
Jessica Schlauderaff <jessicaschlauderaff@gmail.com>
2017-09-10 20:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
   else {
-  var name = katzDeliLine[0]
-  katzDeliLine.shift()
-  return "Currently serving " + name + "."
+    var name = katzDeliLine[0]
+    katzDeliLine.shift()
+    return "Currently serving " + name + "."
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length == 0) {
+    return "The line is currently empty."
+  }
+  else {
+    
+    return "The line is currently: "
   }
 }
